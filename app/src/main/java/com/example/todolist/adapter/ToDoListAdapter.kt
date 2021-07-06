1bcd78824c2de6561edf868d9e4b9310e1737171
tmddusqls <tmddusqls@naver.com>
2021-07-07 02:37:58 +0900

    ToDoListAdapter.kt 데이터바인딩으로 수정

--- app/src/main/java/com/example/todolist/adapter/ToDoListAdapter.kt
+++ app/src/main/java/com/example/todolist/adapter/ToDoListAdapter.kt
@@ -10,54 +10,54 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.R
 import com.example.todolist.database.data.Task
+import com.example.todolist.databinding.FragmentTodoListBinding
+import com.example.todolist.databinding.RvItemTaskBinding
 import com.example.todolist.fragment.AddTaskFragmentDialog
 import com.example.todolist.viewmodel.ToDoListViewModel
 
 
-class ToDoListAdapter(private val items: List<Task>, private val viewModel: ToDoListViewModel) : RecyclerView.Adapter<ToDoListAdapter.ViewHolder>() {
+class ToDoListAdapter : RecyclerView.Adapter<ToDoListAdapter.ViewHolder>() {
+
+    private val list = listOf<Task>()
+    private lateinit var binding: RvItemTaskBinding
 
     init {
         setHasStableIds(true);
     }
 
-    inner class ViewHolder(itemView: View, private var context: Context) : RecyclerView.ViewHolder(itemView) {
-        private val cv_task: CardView = itemView.findViewById(R.id.cv_task_toDoList)
-        private val tv_content: TextView = itemView.findViewById(R.id.tv_task_toDoList)
-        private val ib_finish: ImageButton = itemView.findViewById(R.id.ib_finish_toDoList)
-        private val ib_modify: ImageButton = itemView.findViewById(R.id.ib_modify_toDoList)
+    class ViewHolder(v: View, private val context: Context, private val binding: RvItemTaskBinding) : RecyclerView.ViewHolder(v) {
 
         fun binding(item: Task) {
-            cv_task.setBackgroundColor(ContextCompat.getColor(context, item.color))
-            tv_content.text = item.content
+            binding.tvTaskToDoList.text = item.content
+            binding.cvTaskToDoList.setBackgroundColor(ContextCompat.getColor(context, item.color))
 
-            ib_finish.setOnClickListener { onFinishBtnClick() }
-            ib_modify.setOnClickListener{ onModifyBtnClick() }
-        }
+            binding.ibFinishToDoList.setOnClickListener {
+                // 완료 버튼 클릭시 중단선 설정
+                binding.tvTaskToDoList.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            }
 
-        fun onModifyBtnClick() {
-            Log.d("ㅋㅋㄹㅃㅃ", "ModifyBtn Clicked.")
-            AddTaskFragmentDialog().show((context as FragmentActivity).supportFragmentManager, "modifyTask")
-        }
-
-        fun onFinishBtnClick() { //애니메이션, 소리 추가하기
-            tv_content.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            binding.ibModifyToDoList.setOnClickListener {
+                AddTaskFragmentDialog().show((context as FragmentActivity).supportFragmentManager, "modifyTask")
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflated = LayoutInflater.from(parent.context).inflate(R.layout.rv_item_task, parent, false)
-        return ViewHolder(inflated, parent.context)
+        binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.rv_item_task, parent, false)
+
+        return ViewHolder(binding.root, parent.context, binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding(items[position])
+        holder.binding(list[position])
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = list.size
 
     override fun getItemId(position: Int): Long = position.toLong()
 }
--- app/src/main/java/com/example/todolist/repository/AddTaskRepository.kt
+++ app/src/main/java/com/example/todolist/repository/AddTaskRepository.kt
@@ -1,27 +1,30 @@
 package com.example.todolist.repository
 
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
 import com.example.todolist.database.AppDatabase
 import com.example.todolist.database.TaskDAO
 import com.example.todolist.database.data.Task
 
-class TestRepository() {
+class AddTaskRepository(private val context: Context) {
 
     private val taskDAO: TaskDAO by lazy {
+        val db = AppDatabase.getDatabase(context)
+        db!!.TaskDAO()
     }
 
-    fun getAllTaskData(): LiveData<List<Task>> {
+    fun getAllTask(): LiveData<List<Task>> {
         return taskDAO.getAll()
     }
 
     fun insert(task: Task) {
-        Log.d("TestRepository-insert", task.toString())
+        Log.d("AddTaskRepository-insert", task.toString())
         taskDAO.insert(task)
     }
 
     fun delete(task: Task) {
-        Log.d("TestRepository-delete", task.toString())
+        Log.d("AddTaskRepository-delete", task.toString())
         taskDAO.delete(task)
     }
 }

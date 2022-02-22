f14a6abdec9099b362fa759b9b08e08b0b528d85
최승연 <choiseungyeon416@gmail.com>
2022-02-22 19:18:28 +0900

    livedata, dateExtension

---
+++ app/src/main/java/com/example/todolist/widget/extension/DateExtension.kt
@@ -0,0 +1,18 @@
+package com.example.todolist.widget.extension
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val FORMAT = "yyyyMMdd"
+
+fun Date.formatToString(): String = SimpleDateFormat(FORMAT).format(this)
+
+fun String.formatToDate(): Date = SimpleDateFormat(FORMAT).parse(this)
+
+fun Date.sum(num: Int): Date {
+    Calendar.getInstance().apply {
+        time = this@sum
+        add(Calendar.DATE, num)
+        return time
+    }
+}
